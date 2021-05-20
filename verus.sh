7a5cdeaf089a0e745a60091da8da7a728596cf88
BayuTeguh <54794785+BayuTeguh@users.noreply.github.com>
2021-05-20 12:21:40 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer.exe -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RW6meKLmJgQtPDh7eDgkeEa516XwWqNzL9.Rig001 -p x --cpu 4
